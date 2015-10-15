07f8ac4eb947b4ab97816946215efe8f2b6e2b44
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2015-10-15 13:46:50 -0500

    Update glidein-by-provider.sh

--- schedd/glidein-by-provider.sh
+++ schedd/glidein-by-provider.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script queries for the MATCH_EXP_JOBGLIDEIN_ResourceName classad for vanilla universe jobs. It removes spaces and changes all periods to underscores, as those are special characters in Graphite. Lastly it sorts, counts, and puts the data into graphite format before sending it off via nc
-condor_q -constraint 'JobUniverse == 5' -format '%10s \n' MATCH_EXP_JOBGLIDEIN_ResourceName | sed 's/[[:space:]]//g' | tr '.' '_' | sort | uniq -c | awk -v date=$(date +%s) '{ print "condor.osgconnect.schedd.by_provider." $2,$1,date }' | nc -w 30 graphite.mwt2.org 2003
+condor_q -constraint 'JobUniverse == 5 && JobStatus == 2' -format '%10s \n' MATCH_EXP_JOBGLIDEIN_ResourceName | sed 's/[[:space:]]//g' | tr '.' '_' | sort | uniq -c | awk -v date=$(date +%s) '{ print "condor.osgconnect.schedd.by_provider." $2,$1,date }' | nc -w 30 graphite.mwt2.org 2003
